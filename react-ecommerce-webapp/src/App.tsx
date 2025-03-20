7bfda8ba0b1bdc0bc59f31da8327d474e5268ed7
Evan S Jones <esj.ejones@gmail.com>
2025-03-20 09:43:02 -0400

    third commit, finished navbar

--- react-ecommerce-webapp/src/App.tsx
+++ react-ecommerce-webapp/src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Cart from "./pages/Cart/Cart";
-import { FaShoppingCart } from "react-icons/fa";
+import { NavBar } from "./components/NavBar/NavBar";
+import './App.css'
+
 
 function App() {
   
@@ -9,10 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-          <Link to='/'>Homepage</Link>
-          <Link to='/cart'>
-            <FaShoppingCart />
-          </Link>
+          <NavBar />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/cart' element={<Cart />} />
